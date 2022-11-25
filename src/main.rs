033fd83edf0bd3bacff863e24393c331e70b96c7
splurf <evandotschwartzentruber@gmail.com>
2022-11-25 17:46:48 -0500

    rate limiting implementation

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ mod donut;
 use {
     donut::*,
     std::{
+        collections::HashSet,
         env::args,
         io::{Read, Result, Write},
-        net::{TcpListener, TcpStream, ToSocketAddrs},
+        net::{IpAddr, TcpListener, TcpStream, ToSocketAddrs},
+        sync::{Arc, Mutex},
         thread::{sleep, spawn},
         time::Duration,
     },
@@ -26,10 +28,11 @@ const DELAY: Duration = Duration::from_millis(16);
 /**
  * Verify the potential stream by checking if the User-Agent's product is `curl` and a few other practicalities
  */
-fn verify_stream(stream: Result<TcpStream>) -> Option<TcpStream> {
+fn verify_stream(stream: Result<TcpStream>) -> Option<(IpAddr, TcpStream)> {
     let mut stream = stream.ok()?;
+    let addr = stream.peer_addr().ok()?;
 
-    let mut buf = [0; 100];
+    let mut buf = [0; 96];
     let bytes = stream.read(&mut buf).ok()?;
     let curl = String::from_utf8_lossy(&buf[..bytes]);
     let parts = curl
@@ -47,7 +50,7 @@ fn verify_stream(stream: Result<TcpStream>) -> Option<TcpStream> {
         }
         && parts[2].split_once(" ")?.1.split_once("/")?.0 == "curl"
         && parts[3].split_once(" ")?.1 == "*/*")
-        .then(|| stream)
+        .then(|| (addr.ip(), stream))
 }
 
 /**
@@ -65,14 +68,14 @@ fn handle_stream(mut stream: TcpStream, frames: Vec<Vec<u8>>) -> Result<()> {
 }
 
 fn main() -> Result<()> {
-    //  retrieve specfied address or resort to `localhost`
+    //  retrieve specfied address or default to `localhost`
     let addr = {
         args()
             .nth(1)
             .unwrap_or("localhost:80".to_string())
             .to_socket_addrs()?
-            .find(|sa| sa.port() == 80)
-            .expect("Invalid Port (must use 80)")
+            .next()
+            .expect("Invalid provided address")
     };
 
     //  initiate the listener
@@ -81,13 +84,26 @@ fn main() -> Result<()> {
     //  generate the donuts
     let frames = donuts();
 
-    //  iterate through all incoming streams while verfying in the progress
-    for stream in server.incoming().filter_map(verify_stream) {
-        //  the designated cloned frames for the verifed stream
-        let frames_thread = frames.clone();
+    //  used to rate limit each stream to one session at a time
+    let streams = Arc::new(Mutex::new(HashSet::<IpAddr>::default()));
 
-        //  have another thread handle the verified stream
-        spawn(|| handle_stream(stream, frames_thread));
+    //  iterate through all incoming streams while verfying in the progress
+    for (addr, stream) in server.incoming().filter_map(verify_stream) {
+        //  insert the stream's address into `streams` then continue if it didn't previously exist
+        if streams.lock().unwrap().insert(addr) {
+            let streams_thread = streams.clone();
+
+            //  the designated cloned frames for the verifed stream
+            let frames_thread = frames.clone();
+
+            //  have another thread handle the verified stream
+            spawn(move || {
+                let result = handle_stream(stream, frames_thread);
+                //  remove stream`s address from `streams`
+                streams_thread.lock().unwrap().remove(&addr);
+                result
+            });
+        }
     }
     Ok(())
 }

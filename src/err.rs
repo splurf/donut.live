fd95e090ccd9b4fb682a52c1e2f181645af84a50
splurf <evandotschwartzentruber@gmail.com>
2023-08-02 18:44:57 +0000

    refactoring and improved error handling

---
+++ src/err.rs
@@ -0,0 +1,91 @@
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+pub enum Header {
+    Method,
+    Path,
+    Version,
+    UserAgent,
+    Accept,
+}
+
+impl std::fmt::Display for Header {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self {
+            Self::Method => "Method",
+            Self::Path => "Path",
+            Self::Version => "Version",
+            Self::UserAgent => "User-Agent",
+            Self::Accept => "Accept",
+        })
+    }
+}
+
+pub enum Invalid {
+    Header(Header),
+    Format,
+}
+
+impl ToString for Invalid {
+    fn to_string(&self) -> String {
+        match self {
+            Self::Header(h) => format!("Invalid `{}` header value", h),
+            Self::Format => "Invalid http format".to_string(),
+        }
+    }
+}
+
+impl From<Header> for Invalid {
+    fn from(value: Header) -> Self {
+        Self::Header(value)
+    }
+}
+
+pub enum Error {
+    IO(std::io::Error),
+    HttpParse(httparse::Error),
+    UriParse(uriparse::PathError),
+    Misc(Invalid),
+}
+
+impl From<std::io::Error> for Error {
+    fn from(value: std::io::Error) -> Self {
+        Self::IO(value)
+    }
+}
+
+impl From<httparse::Error> for Error {
+    fn from(value: httparse::Error) -> Self {
+        Self::HttpParse(value)
+    }
+}
+
+impl From<uriparse::PathError> for Error {
+    fn from(value: uriparse::PathError) -> Self {
+        Self::UriParse(value)
+    }
+}
+
+impl<T: Into<Invalid>> From<T> for Error {
+    fn from(value: T) -> Self {
+        Self::Misc(value.into())
+    }
+}
+
+impl std::fmt::Debug for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::fmt::Display::fmt(&self, f)
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&match self {
+            Self::IO(e) => e.to_string(),
+            Self::HttpParse(e) => e.to_string(),
+            Self::UriParse(e) => e.to_string(),
+            Self::Misc(e) => e.to_string(),
+        })
+    }
+}
+
+impl std::error::Error for Error {}

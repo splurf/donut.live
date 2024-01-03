385a317c0e38b75822dba90b320413adafe969db
splurf <evandotschwartzentruber@gmail.com>
2024-01-03 01:58:25 -0500

    docs and minor refac

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,30 +1,35 @@
 use {
-    super::err::Result,
+    crate::Result,
     clap::Parser,
     std::net::{IpAddr, Ipv4Addr, SocketAddr, SocketAddrV4, SocketAddrV6},
     uriparse::Path,
 };
 
+/// A parser containing the configuration for the CLI
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
 pub struct Config {
+    /// IP address
     #[arg(short, long, default_value_t = IpAddr::V4(Ipv4Addr::LOCALHOST))]
     addr: IpAddr,
 
+    /// Port number
     #[arg(short, long, default_value_t = 8080)]
     port: u16,
 
+    /// Location path
     #[arg(long, default_value_t = String::from("/"), value_parser = |path: &str| -> Result<String> {
-        let mut path = Path::try_from(path)?.to_string();
-        if !path.starts_with('/') {
-            path.insert(0, '/')
+        let mut p = Path::try_from(path)?.to_string();
+        if !p.starts_with('/') {
+            p.insert(0, '/');
         }
-        Ok(path)
+        Ok(p)
     })]
     path: String,
 }
 
 impl Config {
+    /// Construct a [`SocketAddr`] from the `addr` and `port` attributes
     pub const fn addr(&self) -> SocketAddr {
         match self.addr {
             IpAddr::V4(ip) => SocketAddr::V4(SocketAddrV4::new(ip, self.port)),
@@ -32,6 +37,7 @@ impl Config {
         }
     }
 
+    /// Return the `path` of the address
     pub fn path(&self) -> &str {
         &self.path
     }
